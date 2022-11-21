c2d95d2f8d262b68bf879ef7efb6b957752b83ab
Vitchet Uk <vitchet.uk@hotmail.com>
2022-11-21 17:26:27 +0100

    lab done

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
+const Tweet = (props) => {
+  return (
+    <div className="tweet">
+      <ProfileImage image={props.tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+          <User userData={props.tweet.user} />
+          <Timestamp time={props.tweet.timestamp} />
+        </div>
+
+        <Message message={props.tweet.message} />
+        <Actions />
+      </div>
+
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+};
+
+export default Tweet;
